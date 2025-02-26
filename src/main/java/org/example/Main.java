c059ae63463c32ebb67cec259baee83646a0df9e
Jeimy Herrera <jeimy493@gmail.com>
2025-02-25 22:40:44 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Ejercicio urna de votaciones
 public class Main {
     public static void main(String[] args) {
         Scanner Entrada = new Scanner(System.in);
